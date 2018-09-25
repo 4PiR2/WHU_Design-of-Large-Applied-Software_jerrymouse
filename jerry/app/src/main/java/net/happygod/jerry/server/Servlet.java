495af329786a78edcf5a4a4673107a6ee2a0b012
CJL <1@1.com>
2018-09-25 17:57:29 +0800

    change package name

--- jerry/app/src/main/java/net/happygod/jerry/server/Servlet.java
+++
@@ -1,8 +0,0 @@
-package net.happygod.jerry.server;
-
-public abstract class Servlet
-{
-	public void init(){}
-	public void doGet(Request request,Response response){}
-	public void doPost(Request request,Response response){}
-}
