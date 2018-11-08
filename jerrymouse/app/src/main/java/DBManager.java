a69e53ed058835c2c6bff90b6c18350d43155d00
853762617@qq.com <39944119+853762617@users.noreply.github.com>
2018-11-08 20:28:45 +0800

    commit

--- jerrymouse/app/src/main/java/DBManager.java
+++ jerrymouse/app/src/main/java/DBManager.java
@@ -24,44 +24,45 @@ public class DBManager extends Servlet
 			String value=request.getParameter(name);
 			map.put(name,value);
 		}
+
 		String database="jerrymouse";
 		String table=map.get("table");
 		String res_sql=new String();
 		String exc_sql=new String();
 		String operation=new String();
-		String message=new String();
+		String message="success!";
 
-		int port=Integer.parseInt(map.get("port"));
-		int proxy=Integer.parseInt(map.get("proxy"));
-		String path=map.get("path");
+		int port;
+		int proxy;
+		String path;
 
-		String uri=map.get("uri");
-		boolean extensive=Boolean.parseBoolean(map.get("extensive"));
-		int permission=Integer.parseInt(map.get("permission"));
-		String authentication=map.get("authentication");
-		boolean directory=Boolean.parseBoolean(map.get("directory"));
-		boolean visible=Boolean.parseBoolean(map.get("visible"));
-		int type=Integer.parseInt(map.get("type"));
-		boolean pathextensive=Boolean.parseBoolean(map.get("pathextensive"));
+		String uri;
+		boolean extensive;
+		int permission;
+		String authentication;
+		boolean directory;
+		boolean visible;
+		int type;
+		boolean pathextensive;
 
-		int code=Integer.parseInt(map.get("code"));
-		int version=Integer.parseInt(map.get("version"));
+		int code;
+		int version;
 
-		int old_port=Integer.parseInt(map.get("old_port"));
-		int old_proxy=Integer.parseInt(map.get("old_proxy"));
-		String old_path=map.get("old_path");
+		int old_port;
+		int old_proxy;
+		String old_path;
 
-		String old_uri=map.get("old_uri");
-		boolean old_extensive=Boolean.parseBoolean(map.get("old_extensive"));
-		int old_permission=Integer.parseInt(map.get("old_permission"));
-		String old_authentication=map.get("old_authentication");
-		boolean old_directory=Boolean.parseBoolean(map.get("old_directory"));
-		boolean old_visible=Boolean.parseBoolean(map.get("old_visible"));
-		int old_type=Integer.parseInt(map.get("old_type"));
-		boolean old_pathextensive=Boolean.parseBoolean(map.get("old_pathextensive"));
+		String old_uri;
+		boolean old_extensive;
+		int old_permission;
+		String old_authentication;
+		boolean old_directory;
+		boolean old_visible;
+		int old_type;
+		boolean old_pathextensive;
 
-		int old_code=Integer.parseInt(map.get("old_code"));
-		int old_version=Integer.parseInt(map.get("old_version"));
+		int old_code;
+		int old_version;
 
 		switch(table)
 		{
@@ -70,6 +71,14 @@ public class DBManager extends Servlet
 				{
 					if(map.containsKey("old_proxy"))
 					{
+						port=Integer.parseInt(map.get("port"));
+						proxy=Integer.parseInt(map.get("proxy"));
+						path=map.get("path");
+
+						old_port=Integer.parseInt(map.get("old_port"));
+						old_proxy=Integer.parseInt(map.get("old_proxy"));
+						old_path=map.get("old_path");
+
 						operation="update";
 						exc_sql="UPDATE general SET port="+port+", proxy="+proxy+", path='"+path+"' " +
 								"WHERE port="+old_port+" AND proxy="+old_proxy+" AND path='"+old_path+"';";
@@ -77,6 +86,10 @@ public class DBManager extends Servlet
 					}
 					else
 					{
+						port=Integer.parseInt(map.get("port"));
+						proxy=Integer.parseInt(map.get("proxy"));
+						path=map.get("path");
+
 						operation="insert";
 						exc_sql="INSERT INTO general (port,proxy,path) VALUES ("+port+","+proxy+",'"+path+"');";
 						res_sql="SELECT * FROM general WHERE port="+port+" AND proxy="+proxy+" AND path='"+path+"';";
@@ -84,6 +97,10 @@ public class DBManager extends Servlet
 				}
 				else if(map.containsKey("old_proxy"))
 				{
+					old_port=Integer.parseInt(map.get("old_port"));
+					old_proxy=Integer.parseInt(map.get("old_proxy"));
+					old_path=map.get("old_path");
+
 					operation="delete";
 					exc_sql="DELETE FROM general WHERE port="+old_port+" AND proxy="+old_proxy+" AND path='"+old_path+"';";
 					res_sql="SELECT * FROM general WHERE 0;";
@@ -100,6 +117,28 @@ public class DBManager extends Servlet
 				{
 					if(map.containsKey("old_uri"))
 					{
+						port=Integer.parseInt(map.get("port"));
+						uri=map.get("uri");
+						extensive=Boolean.parseBoolean(map.get("extensive"));
+						permission=Integer.parseInt(map.get("permission"));
+						authentication=map.get("authentication");
+						directory=Boolean.parseBoolean(map.get("directory"));
+						visible=Boolean.parseBoolean(map.get("visible"));
+						type=Integer.parseInt(map.get("type"));
+						path=map.get("path");
+						pathextensive=Boolean.parseBoolean(map.get("pathextensive"));
+
+						old_port=Integer.parseInt(map.get("old_port"));
+						old_path=map.get("old_path");
+						old_uri=map.get("old_uri");
+						old_extensive=Boolean.parseBoolean(map.get("old_extensive"));
+						old_permission=Integer.parseInt(map.get("old_permission"));
+						old_authentication=map.get("old_authentication");
+						old_directory=Boolean.parseBoolean(map.get("old_directory"));
+						old_visible=Boolean.parseBoolean(map.get("old_visible"));
+						old_type=Integer.parseInt(map.get("old_type"));
+						old_pathextensive=Boolean.parseBoolean(map.get("old_pathextensive"));
+
 						operation="update";
 						exc_sql="UPDATE link SET port="+port+", uri='"+uri+"', extensive="+extensive+", permission="+permission+", " +
 								"authentication='"+authentication+"', directory="+directory+", visible="+visible+", type="+type+", " +
@@ -113,6 +152,17 @@ public class DBManager extends Servlet
 					}
 					else
 					{
+						port=Integer.parseInt(map.get("port"));
+						uri=map.get("uri");
+						extensive=Boolean.parseBoolean(map.get("extensive"));
+						permission=Integer.parseInt(map.get("permission"));
+						authentication=map.get("authentication");
+						directory=Boolean.parseBoolean(map.get("directory"));
+						visible=Boolean.parseBoolean(map.get("visible"));
+						type=Integer.parseInt(map.get("type"));
+						path=map.get("path");
+						pathextensive=Boolean.parseBoolean(map.get("pathextensive"));
+
 						operation="insert";
 						exc_sql="INSERT INTO link (port,uri,extensive,authentication,directory,visible,type,path,pathextensive) " +
 								"VALUES ("+port+", '"+uri+"', "+extensive+", "+permission+", '"+authentication+"', "+directory+", "+visible+", "+type+", '"+path+"', "+pathextensive+") ;";
@@ -123,6 +173,17 @@ public class DBManager extends Servlet
 				}
 				else if(map.containsKey("old_uri"))
 				{
+					old_port=Integer.parseInt(map.get("old_port"));
+					old_path=map.get("old_path");
+					old_uri=map.get("old_uri");
+					old_extensive=Boolean.parseBoolean(map.get("old_extensive"));
+					old_permission=Integer.parseInt(map.get("old_permission"));
+					old_authentication=map.get("old_authentication");
+					old_directory=Boolean.parseBoolean(map.get("old_directory"));
+					old_visible=Boolean.parseBoolean(map.get("old_visible"));
+					old_type=Integer.parseInt(map.get("old_type"));
+					old_pathextensive=Boolean.parseBoolean(map.get("old_pathextensive"));
+
 					operation="delete";
 					exc_sql="DELETE FROM link WHERE port="+old_port+" AND uri='"+old_uri+"' AND extensive="+old_extensive+" AND " +
 							"permission="+old_permission+" AND authentication='"+old_authentication+"' AND directory="+old_directory+" AND" +
@@ -141,6 +202,12 @@ public class DBManager extends Servlet
 				{
 					if(map.containsKey("old_code"))
 					{
+						code=Integer.parseInt(map.get("code"));
+						path=map.get("path");
+
+						old_code=Integer.parseInt(map.get("old_code"));
+						old_path=map.get("old_path");
+
 						operation="update";
 						exc_sql="UPDATE error SET code="+code+", path='"+path+"' " +
 								"WHERE code="+old_code+" AND path='"+old_path+"';";
@@ -148,6 +215,9 @@ public class DBManager extends Servlet
 					}
 					else
 					{
+						code=Integer.parseInt(map.get("code"));
+						path=map.get("path");
+
 						operation="insert";
 						exc_sql="INSERT INTO error (code,path) VALUES ("+code+",'"+path+"');";
 						res_sql="SELECT * FROM error WHERE ocde="+code+" AND path='"+path+"';";
@@ -155,6 +225,9 @@ public class DBManager extends Servlet
 				}
 				else if(map.containsKey("old_code"))
 				{
+					old_code=Integer.parseInt(map.get("old_code"));
+					old_path=map.get("old_path");
+
 					operation="delete";
 					exc_sql="DELETE FROM error WHERE code="+old_code+" AND path='"+old_path+"';";
 					res_sql="SELECT * FROM error WHERE 0;";
@@ -171,12 +244,18 @@ public class DBManager extends Servlet
 				{
 					if(map.containsKey("old_version"))
 					{
+						version=Integer.parseInt(map.get("version"));
+
+						old_version=Integer.parseInt(map.get("old_version"));
+
 						operation="update";
 						exc_sql="UPDATE lock SET version="+version+" WHERE version="+old_version+" ;";
 						res_sql="SELECT * FROM lock WHERE version="+version+" ;";
 					}
 					else
 					{
+						version=Integer.parseInt(map.get("version"));
+
 						operation="insert";
 						exc_sql="INSERT INTO lock (version) VALUES ("+version+") ;";
 						res_sql="SELECT * FROM lock WHERE version="+version+" ;";
@@ -184,6 +263,8 @@ public class DBManager extends Servlet
 				}
 				else if(map.containsKey("old_version"))
 				{
+					old_version=Integer.parseInt(map.get("old_version"));
+
 					operation="delete";
 					exc_sql="DELETE FROM lock WHERE version="+old_version+" ;";
 					res_sql="SELECT * FROM lock WHERE 0;";
@@ -202,7 +283,10 @@ public class DBManager extends Servlet
 		Database db=new Database(database);
 		try
 		{
-			db.execSQL(exc_sql);
+			if(!operation.equals("select"))
+			{
+				db.execSQL(exc_sql);
+			}
 		}
 		catch(Exception e)
 		{
@@ -217,7 +301,6 @@ public class DBManager extends Servlet
 			for(String column : result.columns)
 			{
 				l.add(column);
-
 			}
 			js.put("columns",l);
 			List newlist=new ArrayList();
@@ -227,7 +310,7 @@ public class DBManager extends Servlet
 			}
 			js.put("values",newlist);
 			JSONObject newjson = new JSONObject(js);
-			out.println(newjson);
+			out.println(newjson.toString());
 		}
 		db.close();
 		out.flush();
